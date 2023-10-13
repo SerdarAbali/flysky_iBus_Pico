aad709e1448ad12fc1bfd6ed1553d9b007c2c2e4
Serdar <serdar@bitpixel.fi>
2023-10-13 15:36:32 +0300

    Update iBUS_to_BLDC_ESC.py
    safety measures added (channel 3 default value 1000, stick is all the way down)

--- iBUS_to_BLDC_ESC.py
+++ iBUS_to_BLDC_ESC.py
@@ -6,7 +6,8 @@ class IBUS:
         self.uart = uart
         self.channels = bytearray(14 * 2)  # 2 bytes per channel
         for i in range(0, 28, 2):
-            self.channels[i:i+2] = bytes([0xDC, 0x05])  # Default to 1500
+            self.channels[i:i+2] = bytes([0xDC, 0x05])  # Default to 1500 for all channels
+        self.channels[4:6] = bytes([0xE8, 0x03])  # Set channel 3 default to 1000
         self.last_received = time.ticks_ms()
         self.timeout = 500  # Timeout in milliseconds
 
@@ -14,15 +15,15 @@ class IBUS:
         data_length = self.uart.any()
         if data_length >= 32:
             data = self.uart.read(data_length)
-            # Check for IBUS header bytes
             if data[0] == 0x20 and data[1] == 0x40:
-                self.channels[:28] = data[2:30]  # Update all channels
+                self.channels[:28] = data[2:30]
                 self.last_received = time.ticks_ms()
 
     def check_timeout(self):
         if time.ticks_diff(time.ticks_ms(), self.last_received) > self.timeout:
             for i in range(0, 28, 2):
-                self.channels[i:i+2] = bytes([0xDC, 0x05])  # Reset to 1500
+                self.channels[i:i+2] = bytes([0xDC, 0x05])
+            self.channels[4:6] = bytes([0xE8, 0x03])  # Reset channel 3 to 1000
 
     def get_channel(self, channel):
         if 1 <= channel <= 14:
@@ -30,21 +31,12 @@ class IBUS:
         else:
             return None
 
-# Setup UART for RX, it is the very last pin in iBus slot (servo texted) (TX slot only for receiving sensor telemetry back to transmitter)
 uart = machine.UART(0, baudrate=115200, rx=machine.Pin(1))
-
-# Setup PWM for BLDC ESC
-esc_pwm_pin = machine.Pin(2)  # GP2 pin on pico
-esc_pwm = machine.PWM(esc_pwm_pin)
-esc_pwm.freq(50)  # Set frequency to 50Hz or test frequencies and decide what your ESC more proficient with
-
-ibus = IBUS(uart)
+gp3 = machine.Pin(3, machine.Pin.IN)
 
 def process_throttle_value(value):
-    # Convert the channel value (typically 1000 to 2000) to a PWM duty cycle
-    # This conversion might need adjustments based on the ESC's expectations
     min_duty = 0
-    max_duty = 65025  # For 100% duty cycle with 16-bit resolution
+    max_duty = 65025
     return int((value - 1000) / 1000 * (max_duty - min_duty) + min_duty)
 
 def get_switch_status(value):
@@ -58,32 +50,46 @@ def get_switch_status(value):
 def calculate_duty_percentage(duty_value, max_duty=65025):
     return (duty_value / max_duty) * 100
 
+ibus = IBUS(uart)
+
 while True:
     ibus.read_channels()
     ibus.check_timeout()
     
-    # Process throttle value for BLDC ESC
-    throttle_value = process_throttle_value(ibus.get_channel(3))
-    esc_pwm.duty_u16(throttle_value)
+    ch1_value = ibus.get_channel(1)
+    if 1000 <= ch1_value <= 2000:
+        if gp3.value() == machine.Pin.IN:
+            gp3.init(mode=machine.Pin.OUT)
+            esc_pwm_pin = machine.Pin(2, machine.Pin.OUT)
+            esc_pwm = machine.PWM(esc_pwm_pin)
+            esc_pwm.freq(50)
+            neutral_duty = process_throttle_value(1500)
+            esc_pwm.duty_u16(neutral_duty)
+        
+        throttle_value = process_throttle_value(ibus.get_channel(3))
+        esc_pwm.duty_u16(throttle_value)
+        
+        duty_percentage = calculate_duty_percentage(throttle_value)
+        
+        swa_status = get_switch_status(ibus.get_channel(7))
+        swb_status = get_switch_status(ibus.get_channel(8))
+        swc_status = get_switch_status(ibus.get_channel(9))
+        swd_status = get_switch_status(ibus.get_channel(10))
+        
+        print("Ch1: {} | Ch2: {} | Ch3: {} (duty: {:.2f}%) | Ch4: {} | SWA: {} | SWB: {} | SWC: {} | SWD: {}".format(
+            ibus.get_channel(1),
+            ibus.get_channel(2),
+            ibus.get_channel(3),
+            duty_percentage,
+            ibus.get_channel(4),
+            swa_status,
+            swb_status,
+            swc_status,
+            swd_status
+        ), end='\r')
+    else:
+        if gp3.value() == machine.Pin.OUT:
+            gp3.init(mode=machine.Pin.IN)
     
-    # Calculate duty cycle percentage
-    duty_percentage = calculate_duty_percentage(throttle_value)
-    
-    # Print switch statuses
-    swa_status = get_switch_status(ibus.get_channel(7))
-    swb_status = get_switch_status(ibus.get_channel(8))
-    swc_status = get_switch_status(ibus.get_channel(9))
-    swd_status = get_switch_status(ibus.get_channel(10))
-    
-    print("Ch1: {} | Ch2: {} | Ch3: {} (duty: {:.2f}%) | Ch4: {} | SWA: {} | SWB: {} | SWC: {} | SWD: {}".format(
-        ibus.get_channel(1),
-        ibus.get_channel(2),
-        ibus.get_channel(3),
-        duty_percentage,
-        ibus.get_channel(4),
-        swa_status,
-        swb_status,
-        swc_status,
-        swd_status
-    ), end='\r')
     time.sleep(0.1)
+
